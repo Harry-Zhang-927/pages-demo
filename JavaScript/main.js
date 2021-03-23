298f083b83cdc27273d52ab3413a72637b5229fe
Harry-Zhang-927 <i200011@e.ntu.edu.sg>
2021-03-23 21:40:25 +0800

    add web page

---
+++ JavaScript/main.js
@@ -0,0 +1,20 @@
+console.log("Hello11".length);
+console.log("hello".charAt(0));
+console.log("Hello,Hello,HelloHello,world".replace("Hello","goodbye"));
+var name ="DasAuto"; /*var variable*/
+console.log(name);
+name = "Harry"
+console.log(name);
+const Pi=3.14; /*const constant, unchangable*/
+var x="2"+"3";//be careful
+console.log(x);
+var x=1+"2";//be careful
+console.log(x);
+var x=1+3+"2";//be careful
+console.log(x);
+console.log(123=="123") //"123" "123" true;
+console.log(123==="123") //123 "123" false;
+
+
+
+ 
